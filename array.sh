7d02b1315f719fceb540149fcc820890f5746c51
“shashankvirat” <shashankcgowda81@gmail.com>
2022-03-14 03:50:26 +0000

    array script

---
+++ array.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in 5,100,200
+do
+	echo $i
+done
---
+++ switch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "enter the input value"
+read var
+case $var in
+	1) echo " the value is 1"
+		;;
+	2) echo " the value is 2"
+		;;
+	'abc') echo "the value is abc"
+		;;
+		*) echo "the value is othere that 1,2,abc"
+		;;
+esac
